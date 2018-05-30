808ffdbe6f0dd0a7da6142aa1d47f84b0cd601e8
quest1onmark <jonas.schultheiss3@gmail.com>
2018-05-30 14:26:53 +0200

    important

--- app.js
+++ app.js
@@ -5,5 +5,5 @@ request({
     url: `https://maps.googleapis.com/maps/api/geocode/json?address=${settings.address}&key=${settings.apiKey}`,
     json: true
 }, (error, response, body) => {
-    console.log(body);
+    console.log(JSON.stringify(body, undefined, 2));
 });
