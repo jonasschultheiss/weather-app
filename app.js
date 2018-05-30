6ec1ad0809290b5e1398be4690f54aa9ddd6f138
quest1onmark <jonas.schultheiss3@gmail.com>
2018-05-30 16:37:51 +0200

    added yargs support

--- app.js
+++ app.js
@@ -1,9 +1,25 @@
 const request = require('request');
 const settings = require('./settings.js');
+const yargs = require('yargs');
+
+const argv = yargs
+    .options({
+        a: {
+            demand: true,
+            alias: 'Address',
+            describe: 'Address to fetch weather for',
+            string: true
+        }
+    })
+    .help()
+    .alias('help', 'h')
+    .argv;
 
 request({
     url: `https://maps.googleapis.com/maps/api/geocode/json?address=${settings.address}&key=${settings.apiKey}`,
     json: true
 }, (error, response, body) => {
-    console.log(JSON.stringify(body, undefined, 2));
+    console.log(`Address: ${body.results[0].formatted_address}`);
+    console.log(`lat: ${body.results[0].geometry.location.lat}`);
+    console.log(`lng: ${body.results[0].geometry.location.lng0}`);
 });
