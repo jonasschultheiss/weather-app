94c7965a62d42b4c74a5824a9337cb0bcc15c99b
questionmark <jonas.raphael.schultheiss@gmail.com>
2018-06-05 10:54:40 +0000

    made some basic changes

--- app.js
+++ app.js
@@ -21,5 +21,5 @@ request({
 }, (error, response, body) => {
     console.log(`Address: ${body.results[0].formatted_address}`);
     console.log(`lat: ${body.results[0].geometry.location.lat}`);
-    console.log(`lng: ${body.results[0].geometry.location.lng0}`);
+    console.log(`lng: ${body.results[0].geometry.location.lng}`);
 });
